aa4045249ae736709eadb02f17c61d117b5a7da0
Ryan <rbergeson24@gmail.com>
2015-04-08 11:52:39 -0700

    Added Keydown

--- js/app.js
+++ js/app.js
@@ -23,7 +23,19 @@ $(document).ready(function(){
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
     });
-
+     $(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+      playCool();
+      $('.ryu-throwing').hide();
+      $('.ryu-cool').show();
+    }   
+  }).keyup(function(e) {
+    if (e.keyCode == 88) {
+      $('#cool')[0].pause();
+      $('#cool')[0].load();
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    }
 });
 
 function playHadouken() {
